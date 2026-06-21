6e157748a82bf680e0acecf7b82042ae887ad056
ion-117 <iancredibleplay@icloud.com>
2026-06-21 17:54:48 -0400

    window creation

--- Engine/src/main.cpp
+++ Engine/src/main.cpp
@@ -1,48 +1,49 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);  
 
 int main()
 {
-  if (!glfwInit())
-  {
-    return -1;
-  }
-
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow *window = glfwCreateWindow(800, 600, "OpenGL Test", nullptr, nullptr);
 
-  if (!window)
-  {
+
+GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+if (window == NULL)
+{
+    std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
     return -1;
-  }
-
-  glfwMakeContextCurrent(window);
-
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-  {
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    return -1;
-  }
-
-  glViewport(0, 0, 800, 600);
-
-  while (!glfwWindowShouldClose(window))
-  {
-    glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
-
-  glfwDestroyWindow(window);
-  glfwTerminate();
-
-  return 0;
 }
-//hi
+glfwMakeContextCurrent(window);
+
+if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+{
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+}    
+
+glViewport(0, 0, 800, 600);
+
+glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
+
+
+while(!glfwWindowShouldClose(window))
+{
+    glfwSwapBuffers(window);
+    glfwPollEvents();    
+}
+
+glfwTerminate();
+return 0;
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+} 
